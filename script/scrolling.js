d19d682b07163d15ccd6569cb1d2a29645582bdc
akademuk <akademuk24@gmail.com>
2023-01-18 09:04:16 +0200

    ss

--- script/scrolling.js
+++ script/scrolling.js
@@ -1,33 +1,34 @@
+// let currentLocation = 'firstPage';
+// let autoScrolling = false;
 
-$(document).ready(function(){
-let currentLocation = 'firstPage';
-let autoScrolling = false;
+// let [firstHeight,secondHeight,thirdHeight] = [$('#firstPage').offset().top,
+//  $('#secondPage').offset().top,
+//  $('#thirdPage').offset().top];
 
-let [firstHeight,secondHeight,thirdHeight] = [$('#firstPage').offset().top,
- $('#secondPage').offset().top,
- $('#thirdPage').offset().top];
+// $(document).scroll(e => {
+//     let scrolled = $(window).scrollTop();
+//     // Если user поскроллил
+//     if (!autoScrolling) {
+//     	if (scrolled > 1 && currentLocation == 'firstPage') {
+//             scrollPage(secondHeight, 'secondPage');
+//         } else if (scrolled > secondHeight + 1 && currentLocation == 'secondPage') {
+//             scrollPage(thirdHeight, 'thirdPage');
+//         } else if (scrolled < thirdHeight - 1 && currentLocation == 'thirdPage') {
+//             scrollPage(secondHeight, 'secondPage');
+//         } else if (scrolled < secondHeight - 1 && currentLocation == 'secondPage') {
+//             scrollPage(firstHeight, 'firstPage');
+//         }
+//     }
 
-$(window).scroll(e => {
-    let scrolled = $(window).scrollTop();
-    // Если user поскроллил
-    if (!autoScrolling) {
-    	if (scrolled > 1 && currentLocation == 'firstPage') {
-            scrollPage(secondHeight, 'secondPage');
-        } else if (scrolled > secondHeight && currentLocation == 'secondPage') {
-            scrollPage(thirdHeight, 'thirdPage');
-        } else if (scrolled < thirdHeight && currentLocation == 'thirdPage') {
-            scrollPage(secondHeight, 'secondPage');
-        } else if (scrolled < secondHeight && currentLocation == 'secondPage') {
-            scrollPage(firstHeight, 'firstPage');
-        }
-    }
+//     function scrollPage(nextHeight, page){
+//       currentLocation = page; autoScrolling = true;
+//       $('body,html').animate({scrollTop:nextHeight}, 500, () => {
+//           autoScrolling = false;
+//       });
+//     }
+// })
 
-    function scrollPage(nextHeight, page){
-      currentLocation = page; autoScrolling = true;
-      $('body,html').animate({scrollTop:nextHeight}, 500, () => {
-          autoScrolling = false;
-      });
-    }
-})
-});
+
+
+
 
