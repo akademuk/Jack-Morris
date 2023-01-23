391513415594baf88e038850306cb9345ec6dadb
akademuk <akademuk24@gmail.com>
2023-01-23 09:34:27 +0200

    sss

--- script/select.js
+++ script/select.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
-  $(".accordion").on("click", ".heading", function() {
-
-  $(this).toggleClass("active").next().slideToggle().css('display', 'flex');;
-
-  $(".contents").not($(this).next()).slideUp(300);
-               
+  $(".accordion1").on("click", ".heading1", function() {
+  $(this).toggleClass("active").next().slideToggle();
+  $(".contents1").not($(this).next()).slideUp(300); 
   $(this).siblings().removeClass("active");
   });
- });
-      
+
+  $(".option").click(function() {
+    $(".option").removeClass("active").eq($(this).index()).addClass("active");
+    $(".heading1").hide().eq($(this).index()).fadeIn()
+  }).eq(0).addClass("active");
+});
--- script/tabs.js
+++ script/tabs.js
@@ -4,7 +4,29 @@ $(".tab").click(function() {
 }).eq(0).addClass("active");
 
 
-// $(".tabsOption").click(function() {
-// 	$(".tabsOption").removeClass("active").eq($(this).index()).addClass("active");
-// 	$(".tab_items").hide().eq($(this).index()).fadeIn()
+let index = 1;
+
+const on = (listener, query, fn) => {
+	document.querySelectorAll(query).forEach(item => {
+		item.addEventListener(listener, el => {
+			fn(el);
+		})
+	})
+}
+
+on('click', '.selectBtn', item => {
+	const next = item.target.nextElementSibling;
+	next.classList.toggle('toggle');
+	next.style.zIndex = index++;
+});
+on('click', '.option', item => {
+	item.target.parentElement.classList.remove('toggle');
+
+	const parent = item.target.closest('.select').children[0];
+	parent.setAttribute('data-type', item.target.getAttribute('data-type'));
+	parent.innerText = item.target.innerText;
+})
+
+// $(".option").click(function() {
+// 	$(".popupContentsDetailed").addClass("box");
 // }).eq(0).addClass("active");
